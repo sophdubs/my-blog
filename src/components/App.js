9df045c63a234f98a5a60e49e7b1df4c3d00acfd
Sophie Dubois <sophiemargueritedubois@gmail.com>
2020-01-15 16:00:51 -0800

    Now fetching articles from db in app component and updating state. Then passing state to ArticleLink component

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,31 @@
 import React from "react";
 import ArticleLink from "./ArticleLink";
+import db from "../db/db";
 
 class App extends React.Component {
   state = {
-    articles: {}
+    articles: []
   };
+
+  componentDidMount() {
+    let articles = [];
+    db.collection("articles")
+      .get()
+      .then(querySnapshot => {
+        querySnapshot.forEach(doc => {
+          articles.push(doc.data());
+        });
+      })
+      .then(() => {
+        this.setState({ articles: articles });
+      });
+  }
   render() {
     return (
       <div className="App">
         <h1 className="title">My Blog</h1>
         <h4 className="name">Sophie Dubois</h4>
-        <ArticleLink />
+        <ArticleLink articles={this.state.articles} />
       </div>
     );
   }
